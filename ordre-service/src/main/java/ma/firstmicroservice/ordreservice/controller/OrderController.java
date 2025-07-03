634f861b6b1dfb662db710092c8ffdb171899b66
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-07-03 12:31:00 +0100

    debug: fixed CircuitBreaker fallback issue
    Changed fallback method to match return type and fixed the problem

--- ordre-service/src/main/java/ma/firstmicroservice/ordreservice/controller/OrderController.java
+++ ordre-service/src/main/java/ma/firstmicroservice/ordreservice/controller/OrderController.java
@@ -1,11 +1,14 @@
 package ma.firstmicroservice.ordreservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ma.firstmicroservice.ordreservice.dto.OrderRequest;
 import ma.firstmicroservice.ordreservice.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 
 @RestController
@@ -16,6 +19,7 @@ public class OrderController {
     private OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String saveOrder(@RequestBody OrderRequest orderRequest) {
         orderService.saveOrder(orderRequest);
         log.info("Saving Order");
@@ -24,6 +28,11 @@ public class OrderController {
     @GetMapping
     public String hello() {
         return "Hello from OrderController";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        log.info("Cannot Place Order Executing Fallback logic");
+        return "Oops! Something went wrong, please order after some time!";
     }
 
 
